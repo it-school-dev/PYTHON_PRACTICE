c44b889bc59b96b14fdc2946f62faf65f774bd6c
velusya <117757905+velusya@users.noreply.github.com>
2022-11-08 20:53:23 +0300

    Update main.py

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,18 @@
 def hiFunction(name):
     print('Hi, ' + name)
+
+    
+def sum(num1, num2):
+    return num1 + num2
+
+
+def isEven(num):
+    return num % 2 == 0
+
+
+def apples(num):
+    print('i have ' + str(num) + ' apples')
+    
+    
+def getPower(num):
+    return num**2
