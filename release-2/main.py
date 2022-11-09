fb6d189ca2d76d72b92d46f56b0093f0e083150c
VeronaPl <vp31102003@gmail.com>
2022-11-09 06:23:56 +0300

    add func stack

--- release-2/main.py
+++ release-2/main.py
@@ -17,6 +17,8 @@ def findTheMostReapetedEls(mas):
     arr = max(set(mas), key=mas.count)
     return list(set(x for x in mas if mas.count(x) == mas.count(arr)))
 
+def stack(mas):
+    return mas.count('[') == mas.count(']') if (mas[0] != ']' and mas[-1] != '[') else False
 
 print(*sorting([1, 5, 26, 39, 2], -1))
 print(deepSorting([{'age': 15, 'height': 140}, {'age': 14, 'height': 150}], 'age'))
@@ -24,3 +26,4 @@ print(getNumbers(['d', 1, ['b', 'glfdk'], 3, 0.8]))
 print(_min([1, 5, -222, 34, -100]))
 print(getSet([1, 1, 'h', 1, 3, 4, 'h', 2, 2]))
 print(findTheMostReapetedEls([1, 1, 1, 1, 3, 4, 2, 2, 2, 2]))
+print(stack(['[','[', ']', ']', '[' ]))
