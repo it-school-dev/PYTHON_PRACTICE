cdd023806ae83b250556b62400cc5e081780fea6
VeronaPl <vp31102003@gmail.com>
2022-11-09 04:50:19 +0300

    add deepSorting

--- release-2/main.py
+++ release-2/main.py
@@ -1,4 +1,8 @@
 def sorting(arr, direction):   #Иногда эта любовь к генераторам списков даже пугает...
     return [[x for x in sorted(arr)] if direction == 1 else [x for x in sorted(arr, reverse=True)]]
 
+def deepSorting(mas, atr):
+    return sorted(list(mas), key=lambda item: item[atr])
+
 print(*sorting([1, 5, 26, 39, 2], -1))
+print(deepSorting([{'age': 15, 'height': 140}, {'age': 14, 'height': 150}], 'age'))
