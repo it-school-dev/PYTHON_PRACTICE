a0f9e1b0a42019d0e37ee8eeeda63e094fa216bd
Yulia Byzeeva <yuliabyzeeva@MacBook-Air-Yulia.local>
2022-11-09 18:27:00 +0300

    tried creating Object from String

--- release-3/main.py
+++ release-3/main.py
@@ -2,6 +2,26 @@
 # my_dictionary = {'name': 'Lim', 'surname': 'Bok', 'age': 12}
 # print((my_dictionary['name']) + ' ' + (my_dictionary['surname']))
 
+# функция createObjFromStr
+
+# class Dict2Class(object):
+      
+#     def __init__(self, my_dict):
+          
+#         for key in my_dict:
+#             setattr(self, key, my_dict[key])
+  
+# if __name__ == "__main__":
+      
+#     # Creating the dictionary
+#     my_dict = {"Name": "Vova",
+#                "Age": "12",
+#                "Type": "Owner"}
+      
+#     result = Dict2Class(my_dict)
+    
+#     print(result.Name, result.Age, result.Type)
+#     print(type(result))
 
 
 
