b3c99bbf22c7edb7306f0300f2c7524f3126476c
VeronaPl <vp31102003@gmail.com>
2022-11-09 05:22:20 +0300

    add func getNumbers

--- release-2/main.py
+++ release-2/main.py
@@ -4,5 +4,9 @@ def sorting(arr, direction):   #Иногда эта любовь к генера
 def deepSorting(mas, atr):
     return sorted(list(mas), key=lambda item: item[atr])
 
+def getNumbers(mas):
+    return [x for x in mas if (type(x) == int or type(x) == float)]
+
 print(*sorting([1, 5, 26, 39, 2], -1))
 print(deepSorting([{'age': 15, 'height': 140}, {'age': 14, 'height': 150}], 'age'))
+print(getNumbers(['d', 1, ['b', 'glfdk'], 3, 0.8]))
