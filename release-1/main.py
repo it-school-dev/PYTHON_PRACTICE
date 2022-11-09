b3b90981c2b05490f228d82c43aa0a0a80c8db50
zaiden54 <you@example.com>
2022-11-09 17:13:53 +0300

    resolved realise-1

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,20 @@
 def hiFunction(name):
-    print()
+    return 'Hi, ' + name
+
+def sum(num_1, num_2):
+    return num_1 + num_2
+
+def isEven(num):
+    return num % 2 == 0
+
+def apples(amount):
+    return f'i have {amount} apples'
+
+def getPower(num):
+    return num**2
+
+print(hiFunction('Denis'))
+print(sum(1, 2))
+print(isEven(0))
+print(apples(2))
+print(getPower(4))
