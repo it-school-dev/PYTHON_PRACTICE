af607dd985a31efbe0a21d19223dfd01d8b960c3
velusya <117757905+velusya@users.noreply.github.com>
2022-11-09 22:44:26 +0300

    Update main.py

--- release-3/main.py
+++ release-3/main.py
@@ -9,3 +9,21 @@ def checkForPropertyAndValue(obj, keyname):
         return True
     else:
         return False
+    
+    
+def createObjFromStr(s):
+    res = {}
+    s = s[1:-1]
+    s = s.replace(' ', '')
+    try:
+        for obj in s.split(','):
+            key = obj.split(':')[0]
+            value = obj.split(':')[1]
+            value = value.replace("'", '')
+            try:
+                res[key] = int(value)
+            except:
+                res[key] = value
+        return res
+    except:
+        return "ERROR"
