01d1bc278633d8073cf3a55825e84dc5790006de
Kirill <sakir61033@gmail.com>
2022-11-09 18:35:34 +0300

    some thing done

--- release-2/main.py
+++ release-2/main.py
@@ -1,2 +1,57 @@
-def sorting(arr, direction):
-    return 
+#1
+
+from unittest.util import sorted_list_difference
+
+def sorting(sortedList):
+
+    Input = input('введите числа от 1 до 9 без пробелов и запятых: ')
+    List = list(Input)
+    
+    x = int(input("Введите -1 или 1: "))
+    if x == 1:
+        List.sort()
+        print(List)
+    if x == -1:
+        List.sort(reverse=True)
+        print(List)
+sorting(sorted_list_difference)
+
+#2
+
+def deepsorting(a, b, s):
+    if a[s] > b[s]:
+        print(b,a)
+    else:
+        print(a,b)
+p_1 = {'age': 15, 'height': 140}
+p_2 = {'age': 14, 'height': 150}
+s_1 = 'age'
+deepsorting(p_1,p_2,s_1)
+
+#3
+
+
+
+
+
+
+
+
+
+#4
+
+Input = input()
+list = list(Input)  
+low_num = list[0]
+for i in list:
+    if i < low_num:
+        low_num = i
+print(low_num)
+
+#5
+
+list = input()   
+mySet = set(list)
+print(mySet)
+
+
--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,49 @@
-def fullName(obj):
-    return 
+#1
+
+def fullName(n, s):
+    a = {'name': n, 'surname': s}
+    print(a['surname'].title(), a['name'].title())
+name = input('Введи имя: ')
+surname = input('Введи фамилию: ')
+fullName(name, surname)
+
+#2
+
+
+
+
+
+
+
+#3
+def parser(a, b, c):  
+    list = [a, b, c]
+    for i in list:
+        if i['type'] == 'owner':
+            s = 'name: ' + i['name'] + '\nage: ' + str(i['age'])
+            n = i['name']
+    for i in list:
+        if i['type'] != 'owner' and i['owner'] == n:
+            s = s +  '\n' + i['type'] + 's:' + '\n    [\n' + '    name: ' + i['name'] + '\n    oc: ' + i['oc'] + '\n    ]'
+    print(s)
+x_1 = {'type': 'owner', 'name': 'Vova', 'age': 12}
+x_2 = {'type': 'computer','name': 'someComputer','oc': 'Mac','owner': 'Vova'}
+x_3 = {'type': 'phone','name': 'somePhone','oc': 'ios','owner': 'Vova'}   
+parser(x_1, x_2, x_3)  
+
+#4
+
+def createObjFromStr(string):
+    try:
+        string = string[1:-1]
+        string = string.replace(' ', '')
+        string = string.replace("'", "")
+        key_value = string.split(',')
+        dictionary = {}
+        for i in key_value:
+            key, value = i.split(':')
+            dictionary[key] = value
+        return dictionary
+    except:
+        return 'ERROR'
+print(createObjFromStr("{name: 'Vove', age: 12, type: 'owner'}"))
