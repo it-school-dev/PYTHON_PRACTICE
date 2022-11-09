ee22ce89078250fbefb35c73143ae0786483f39d
VeronaPl <vp31102003@gmail.com>
2022-11-09 04:17:46 +0300

    add func sorting

--- release-2/main.py
+++ release-2/main.py
@@ -1,2 +1,4 @@
-def sorting(arr, direction):
-    return 
+def sorting(arr, direction):   #Иногда эта любовь к генераторам списков даже пугает...
+    return [[x for x in sorted(arr)] if direction == 1 else [x for x in sorted(arr, reverse=True)]]
+
+print(*sorting([1, 5, 26, 39, 2], -1))
