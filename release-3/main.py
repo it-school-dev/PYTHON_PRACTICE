2f13f9c85355c88d2d753ea59da3814fcca33e76
Yulia Byzeeva <yuliabyzeeva@MacBook-Air-Yulia.local>
2022-11-09 18:11:15 +0300

    tried to do release-3

--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,7 @@
-def fullName(obj):
-    return 
+# функция fullName
+# my_dictionary = {'name': 'Lim', 'surname': 'Bok', 'age': 12}
+# print((my_dictionary['name']) + ' ' + (my_dictionary['surname']))
+
+
+
+
