1b914be44e04a79fed20e4dc7d93108911423d0e
VeronaPl <vp31102003@gmail.com>
2022-11-09 06:48:27 +0300

    add func checkForPropertyAndValue

--- release-3/main.py
+++ release-3/main.py
@@ -1,4 +1,8 @@
 def fullName(obj):
     return obj['surname'] + ' ' + obj['name']
 
+def checkForPropertyAndValue(obj, st):
+    return obj[st] > 0 if (st in obj and type(obj[st]) == int) else False
+
 print(fullName({'name': 'Harry', 'surname': 'Potter', 'age': 12}))
+print(checkForPropertyAndValue({'key': None}, 'key'))
