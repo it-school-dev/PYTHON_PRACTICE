664966e14fae903c938fcf092bbca9d95bb0d93b
Konstantin Fedorov <dmifkonfj23@gmail.com>
2022-11-10 18:47:15 +0300

    Task has been completed

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,37 @@
-def hiFunction(name):
-    print()
+# 1)
+def sayHiToAnyone(name):
+  return "Hello, it's nice to meet you, " + name
+
+print(sayHiToAnyone("Nikita"))
+
+
+# 2)
+def summation(value, multiplier):
+  return value + multiplier
+
+print(summation(10, 12))
+
+
+# 3)
+def isEven(value):
+  if value % 2 == 0:
+    return True
+  else:
+    return False
+
+print(isEven(5))
+
+
+#4
+def apples(amount):
+    # return f"I have {amount} apples"
+    return "I have {} apples".format(amount)
+
+print(apples(7))
+
+
+# 5)
+def getPowerRoot(number):
+  return number ** 2
+
+print(getPowerRoot(25))
--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,21 @@
+#1
 def fullName(obj):
-    return 
+    return obj["surname"] + " " + obj["name"]
+
+print(fullName({
+    "name": "Petya",
+     "surname": "Proger",
+      "age": 14
+    }))
+
+
+
+def checkForPropertyAndValue(obj, key):
+    if key in obj and obj[key] > 0:
+        return True
+    else:
+        return False
+
+# print(checkForPropertyAndValue({"key: 5"}, "key")) #True
+print(checkForPropertyAndValue({"key": 5, "ke": 18}, "ke")) #True
+# print(checkForPropertyAndValue({"key: -2"}, "key")) #False
