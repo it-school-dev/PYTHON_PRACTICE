5097036d9d911552fa7e763a9c2e0e60fe104a69
ShirshovDIM <dbezumny@mail.ru>
2022-11-10 20:51:27 +0300

    Practice

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,27 @@
 def hiFunction(name):
-    print()
+    print('Hi, ' + name)
+
+hiFunction('dima')
+
+def summa(a, b):
+    return(a+b)
+
+summa(3,5)
+
+def isEven(a):
+    if a % 2 == 0:
+        print('true')
+    else:
+        print('false')
+
+isEven(43)
+
+def apples(num):
+    print('i have ' + str(num) + ' apples')
+
+apples(2)
+
+def getPower(num):
+    return(num ** 2)
+
+getPower(2)
