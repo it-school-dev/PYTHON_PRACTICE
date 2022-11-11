7bb080c33902bbc25b46649401d57f70f616245f
Ekaterina Filimonova <katya.fil2001@mail.ru>
2022-11-11 04:31:14 +0300

    Release 1

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,33 @@
+# 1
+World = input()
 def hiFunction(name):
-    print()
+    return('Hi,' + name)
+print(hiFunction(World))
+
+# 2
+# 2 = int(input())
+5 = int(input())
+def sum(x, y):
+    return(x + y)
+print(sum(2, 5))
+
+# 3
+z = int(input())
+def isEven(z):
+    if z % 2 == 0:
+        return "true"
+    else:
+        return "false"
+print(isEven(z))
+
+# 4
+a = int(input())
+def apples(a):
+    return "I have", a, "apples"
+print(apples(a))
+
+# 5
+y = int(input())
+def getPower(y):
+    return y ** 2
+print(getPower(y))
--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,10 @@
+# 1
 def fullName(obj):
-    return 
+    return obj['surname'] + ' ' + obj['name']
+
+# 2
+def checkForPropertyAndValue(obj, key):
+     if key in obj and obj[key] > 0:
+         return True
+     else:
+         return False
