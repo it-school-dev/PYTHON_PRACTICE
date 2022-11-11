9ea64f351d2d912dfadf59369bf05b8c3d6a01de
justfloer <aleksa2510@bk.ru>
2022-11-12 00:22:45 +0300

    Release 1

--- release-1/main.py
+++ release-1/main.py
@@ -1,29 +1,30 @@
 #TASK1
-'''def hiFunction(name):
+def hiFunction(name):
     print('Hi, ', name)
-hiFunction('Nikita')'''
+hiFunction('Nikita')
 
 #TASK2
-'''def sum(a,b):
+def sum(a,b):
     print(a+b)
-sum(5,6)'''
+sum(5,6)
+
 
 #TASK3
-'''def isEven(k):
+def isEven(k):
     if k%2==0:
         print('true')
     else:
         print('false')
 isEven(5)
 isEven(0)
-isEven(4)'''
+isEven(4)
 
 #TASK4
-'''def apples(a):
+def apples(a):
     print('i have', a, 'apples')
-apples(2)'''
+apples(2)
 
 #TASK5
-'''def getPower(a):
+def getPower(a):
     print(a*a)
-getPower(4)'''
+getPower(4)
--- release-2/main.py
+++ release-2/main.py
@@ -1,84 +1,88 @@
 #TASK1
-'''def sorting(a, k):
-    if k==1:
-        a.sort()
-    else:
-        a.sort(reverse=True)
-    print(a)
-a1=[1,5,2]
-sorting(a1,-1)'''
+# def sorting(a, k):
+#     if k==1:
+#         a.sort()
+#     else:
+#         a.sort(reverse=True)
+#     print(a)
+# a1=[1,5,2]
+# sorting(a1,-1)
     
 
-#TASK2?????
-# def deepSorting(a):
-# 123
+#TASK2
+# import operator
+# def deepSorting(a, x):
+#     a.sort(key=operator.itemgetter(x))                    #эта штука достает конкретный х из списка
+#     print(a)
+# deepSorting([{'age': 15, 'height': 140}, {'age': 14, 'height': 150}], 'age') 
+
 
     
 #TASK3
-'''def getNumbers(a):
-    b=[]
-    for i in range(len(a)):
-        if type(a[i])==int:
-            b.append(a[i])
-    print(b)
-a1=['d',1,3,'null',8]
-getNumbers(a1)'''
+# def getNumbers(a):
+#     b=[]
+#     for i in range(len(a)):
+#         if type(a[i])==int:
+#             b.append(a[i])
+#     print(b)
+# a1=['d',1,3,'null',8]
+# getNumbers(a1)
 
 #TASK4
-'''def min(a):
-    mn=0
-    for i in range(len(a)-1):
-        if a[i]<a[i+1]:
-            mn=a[i]
-        else:
-            mn=a[i+1]
-    print(mn)
-a1=[1,5,34,-100]
-min(a1)'''
+# def min(a):
+#     mn=0
+#     for i in range(len(a)-1):
+#         if a[i]<a[i+1]:
+#             mn=a[i]
+#         else:
+#             mn=a[i+1]
+#     print(mn)
+# a1=[1,5,34,-100]
+# min(a1)
 
-#TASK5
-'''def getSet(a):
-    b=[]
-    for i in range(len(a)-1):
-        if a[i]!=a[i-1]:
-            b.append(a[i])
-    print(b)
-a1=[1,1,1,3,4,2,2]
-getSet(a1)'''
+# #TASK5
+# def getSet(a):
+#     b=[]
+#     for i in range(len(a)-1):
+#         if a[i]!=a[i-1]:
+#             b.append(a[i])
+#     print(b)
+# a1=[1,1,1,3,4,2,2]
+# getSet(a1)
 
-#TASK6
-'''def findTheMostReapetedEls(a):
-    b=[]
-    for i in range(len(a)-1):
-        if a[i]!=a[i-1]:
-            b.append(a[i])
-    c=[]
-    for i in range(len(b)):
-        k=a.count(b[i])
-        if k>=3:
-            c.append(b[i])
-    print(c)
-a1=[1,1,1,3,4,2,2,2]
-findTheMostReapetedEls(a1)'''
+# #TASK6
+# def findTheMostReapetedEls(a):
+#     b=[]
+#     for i in range(len(a)-1):
+#         if a[i]!=a[i-1]:
+#             b.append(a[i])
+#     c=[]
+#     for i in range(len(b)):
+#         k=a.count(b[i])
+#         if k>=3:
+#             c.append(b[i])
+#     print(c)
+# a1=[1,1,1,3,4,2,2,2]
+# findTheMostReapetedEls(a1)
 
-#TASK7
-'''def stack(a):
-    open=a.count('[')
-    close=a.count(']')
-    if open==close:
-        print('true')
-    else:
-        print('false')
-a1=['[','[',']',']']
-a2=['[','[',']']
-stack(a1)
-stack(a2)'''
+# #TASK7
+# def stack(a):
+#     open=a.count('[')
+#     close=a.count(']')
+#     if open==close:
+#         print('true')
+#     else:
+#         print('false')
+# a1=['[','[',']',']']
+# a2=['[','[',']']
+# stack(a1)
+# stack(a2)
 
-#TASK8
-'''def checkForBadWord(list):
-    if 'Hi' in list:
-        print('true')
-    else:
-        print('false')
-str='Hi, Nikita'
-checkForBadWord(str)'''
+# #TASK8
+# def checkForBadWord(list):
+#     if 'Hi' in list:
+#         print('true')
+#     else:
+#         print('false')
+# str='Hi, Nikita'
+# checkForBadWord(str)
