ce68cff59451549bae872d054bbce81e2b8becf8
iliadurmanov <mr.dia.2003@yandex.ru>
2022-11-11 14:59:56 +0300

    add all

--- release-2/main.py
+++ release-2/main.py
@@ -1,2 +1,78 @@
-def sorting(arr, direction):
-    return 
+#sorting
+num = ([int(i) for i in input().split(", ")])
+
+a = int(input())
+if a == 1:
+    a = 0
+num.sort(reverse = -a)
+
+#getNumbers
+print(num)
+
+import re
+s = [int(s) for s in re.findall(r'\b\d+\b', input(str()))]
+print(s)
+
+#min
+a = [int(i) for i in input().split(", ")]
+
+print(min(a))
+
+#getSet
+a = [int(i) for i in input().split(", ")]
+
+print(set(a))
+
+
+#findTheMostReapetedEls
+a = [int(i) for i in input().split(", ")]
+max = 0
+b = []
+
+for n in a:
+    d = a.count(n)
+    if d > max:
+        max = d
+
+for n in a:
+    if a.count(n) == max:
+        b.append(n)
+
+print(set(b))
+
+#stack
+a = [str(i) for i in input().split()]
+
+count1 = 0
+count2 = 0
+pr = True
+for n in a:
+    if n == '[':
+        count1 = count1 + 1
+    elif n == ']':
+        count1 = count1 - 1
+    if count1 < 0:
+        pr = False
+    
+if count1 == 0 and pr == True:
+    print(a, " -> true")
+else:
+    print(a, " -> false")
+
+#checkForBadWord
+S1 = input(str())
+S2 = input(str())
+
+print(S2 in S1)
+
+def deepsorting(a, b, s):
+    if a[s] > b[s]:
+        print(b,a)
+    else:
+        print(a,b)
+
+d1 = {'age': 15, 'height': 140}
+d2 = {'age': 14, 'height': 150}
+s1 = 'age'
+
+deepsorting(d1,d2,s1)
--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,55 @@
-def fullName(obj):
-    return 
+def fullName(n, s):
+    d = {'name': n, 'surname': s}
+    print(d['surname'], d['name'])
+
+name = input()
+surname = input()
+
+fullName(name, surname)
+
+#checkForPropertyAndValue
+a, b = map(str, input().split(': '))
+d = {a: b}
+
+c = input()
+
+
+if a == c and int(b) > 0:
+    print('true')
+else:
+    print('false')
+
+def parser(a, b, c):
+
+    list = [a, b, c]
+    for i in list:
+        if i['type'] == 'owner':
+            s = 'name: ' + i['name'] + '\nage: ' + str(i['age'])
+            n = i['name']
+
+    for i in list:
+        if i['type'] != 'owner' and i['owner'] == n:
+            s = s +  '\n' + i['type'] + 's:' + '\n    [\n' + '    name: ' + i['name'] + '\n    oc: ' + i['oc'] + '\n    ]'
+    print(s)
+
+x = {'type': 'owner', 'name': 'Vova', 'age': 12}
+y = {'type': 'computer','name': 'someComputer','oc': 'Mac','owner': 'Vova'}
+z = {'type': 'phone','name': 'somePhone','oc': 'ios','owner': 'Vova'}   
+
+parser(x, y, z)  
+
+def createObjFromStr(string):
+    try:
+        string = string[1:-1]
+        string = string.replace(' ', '')
+        string = string.replace("'", "")
+        Keyandvalue = string.split(',')
+        dictionary = {}
+        for i in Keyandvalue:
+            key, value = i.split(':')
+            dictionary[key] = value
+        return dictionary
+    except:
+        return 'ERROR'
+
+print(createObjFromStr("{name: 'Vove', age: 12, type: 'owner'}"))
