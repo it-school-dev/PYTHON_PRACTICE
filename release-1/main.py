5ea44cecb32350b89a5e5b6d03a4c4e04d1e71c7
justfloer <aleksa2510@bk.ru>
2022-11-11 16:42:43 +0300

    clears

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,29 @@
-def hiFunction(name):
-    print()
+#TASK1
+'''def hiFunction(name):
+    print('Hi, ', name)
+hiFunction('Nikita')'''
+
+#TASK2
+'''def sum(a,b):
+    print(a+b)
+sum(5,6)'''
+
+#TASK3
+'''def isEven(k):
+    if k%2==0:
+        print('true')
+    else:
+        print('false')
+isEven(5)
+isEven(0)
+isEven(4)'''
+
+#TASK4
+'''def apples(a):
+    print('i have', a, 'apples')
+apples(2)'''
+
+#TASK5
+'''def getPower(a):
+    print(a*a)
+getPower(4)'''
