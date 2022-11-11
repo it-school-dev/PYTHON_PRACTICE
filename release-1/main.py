fa358da118f42d51154ae1743dd9271dd5b3e0e3
iliadurmanov <mr.dia.2003@yandex.ru>
2022-11-11 14:51:07 +0300

    first re

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,32 @@
 def hiFunction(name):
-    print()
+    print('Hi, ' + name)
+
+inname = input(str())
+hiFunction(inname)
+
+a = [int(i) for i in input().split(", ")]
+print(sum(a))
+
+def isEven(a):
+    n = 0
+    for i in a:
+        if (abs(a[n]) == 1) or (abs(a[n] % 2) == 0):
+            print(a[n], '->', 'true')
+        else:
+            print(a[n], '->', 'false')
+        n = n + 1
+
+numbers =  [int(i) for i in input().split(", ")]
+isEven(numbers)
+
+def apples(num):
+    print('I have ' + num + ' apples')
+
+nums = input(str())
+apples(nums)
+
+def get_power(a):
+    print(a, '->', a ** 2)
+
+b = int(input())
+get_power(b)
