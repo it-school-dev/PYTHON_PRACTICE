36f8ab3bc46f5383352b8b53a0d9025f91928ad2
Ivan Zhbanov <john190703@gmail.com>
2022-11-13 16:33:14 +0300

    add release 3

--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,57 @@
 def fullName(obj):
-    return 
+    return obj['surname']+' '+obj['name']
+
+def checkForPropertyAndValue(obj,key):
+    return (key in obj)and(obj[key]>0)
+
+def parser(arr):
+    own={}
+    for i in arr:
+        if i['type']=='owner':
+            own.update(i)
+        else:
+            del i['owner']
+            own[str(i['type'])+'s']=[]
+    for i in arr:
+        if i['type']!='owner':
+            own[str(i['type'])+'s'].append(i)
+            del i['type']
+    return own
+
+def createObjFromStr(stroka): #Допустимо ли использовать Eval и Exec
+    try:
+        return eval(stroka)
+    except:
+        return "ERROR"
+
+    # obj= eval(stroka)
+    # return obj['age']
+
+# print(fullName({'name': 'Lim', 'surname': 'Bok', 'age': 12}))
+# print(checkForPropertyAndValue({'key': 1}, 'key2'))
+print(parser([
+    {
+        'type': 'computer',
+        'name': 'someComputer',
+        'oc': 'Mac',
+        'owner': 'Vova'
+    },
+    {
+        'type': 'computer',
+        'name': 'newComputer',
+        'oc': 'Windows',
+        'owner': 'Vova'
+    },
+    {
+        'type': 'phone',
+        'name': 'somePhone',
+        'oc': 'ios',
+        'owner': 'Vova'
+    },
+    {
+        'type': 'owner',
+        'name': 'Vova',
+        'age': 12
+    }
+]))
+# print(createObjFromStr('''{'name': 'Vova', 'age': 12, 'type': 'owner'}'''))
