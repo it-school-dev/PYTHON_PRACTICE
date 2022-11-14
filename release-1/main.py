f9c35ccd282a7012a0fc99ab6d1793862c3ff1bd
ayneskkk <ksennny.ya@mail.ru>
2022-11-15 02:39:38 +0500

    неужели первый релиз ксения к

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,26 @@
-def hiFunction(name):
-    print()
+#1.1
+
+def uname(imya):
+    return 'Hi, ' + imya
+
+#1.2
+
+def sum(a, b):
+    return a + b
+
+
+#1.3
+
+def isEven(numChet):
+    return numChet % 2 == 0
+  
+
+#1.4
+
+def apples(fraza):
+    return f'i have {fraza} apples'
+
+#1.5
+
+def getPower(a):
+    return a**2 
