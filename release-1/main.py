37779b352f3b660f6d8f49b5182a495a4ea32063
Lehtero <lehtero@inbox.ru>
2022-11-15 17:00:31 +0300

    Home Task 1 Artamonova release-1

--- release-1/main.py
+++ release-1/main.py
@@ -1,2 +1,17 @@
 def hiFunction(name):
-    print()
+    return 'Hi, ' + name
+
+def sum(a, b):
+    return a+b
+
+def isEven(a):
+    if a%2==0:
+        return 'true'
+    else:
+        return 'false'
+
+def apples(a):
+    return 'I have ' + str(a) + ' apples'
+
+def getPower(a):
+    return a*a
--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,16 @@
-def fullName(obj):
-    return 
+def fullName(d):
+    s = d['name'] + ' ' + d['surname']
+    return s
+
+dict = {'name': 'Lim', 'surname': 'Bok', 'age': 12}
+print(fullName(dict))
+
+def checkForPropertyAndValue(d, kn):
+    return
+
+
+def parser():
+    return
+
+def createObjFromStr():
+    return
